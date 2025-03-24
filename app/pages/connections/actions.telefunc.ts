d2fd8b582b31ef81fe47e1546c6cd69aad513878
kruceo <kruceo.com@gmail.com>
2025-03-24 16:43:50 -0300

    wip: fixes for telefunctions

--- app/pages/connections/actions.telefunc.ts
+++ app/pages/connections/actions.telefunc.ts
@@ -1,40 +1,70 @@
-import { apiUrl } from '../../api/api'
+import { AxiosError } from 'axios'
+import { apiAxios, apiUrl } from '../../api/api'
 
-export async function onBlockAction(connectionId: string, domains: string[], action: "DELETE" | "POST") {
-  const url = apiUrl("v1/connections/" + connectionId + "/blocks")
-  const res = await fetch(url, {
-    method: action, headers: { "Content-Type": "application/json" }, body: JSON.stringify({
-      Names: domains
+export async function onBlockAction(connectionId: string, domains: string[]) {
+  const url = apiUrl(`v1/connections/${connectionId}/blocks`)
+  console.log(url)
+  try {
+    const res = await apiAxios.delete(url, {
+      headers: { "Content-Type": "application/json" },
+      data: { Names: domains },
     })
-  })
+    return await res.data
+  } catch (error) {
+    return (error as AxiosError).response?.data
+  }
 }
 
-export async function onNewRedirectAction(connectionId: string, from: string, recordType: string, to: string,localZone:boolean) {
-  const url = apiUrl("v1/connections/" + connectionId + "/redirects")
-  const res = await fetch(url, {
-    method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({
-      From: from, To: to, RecordType: recordType,LocalZone:localZone
+export async function onNewRedirectAction(connectionId: string, from: string, recordType: string, to: string, localZone: boolean) {
+  const url = apiUrl(`v1/connections/${connectionId}/redirects`)
+  try {
+    const res = await apiAxios.post(url, {
+      From: from,
+      To: to,
+      RecordType: recordType,
+      LocalZone: localZone,
+    }, {
+      headers: { "Content-Type": "application/json" },
     })
-  })
+    return await res.data
+  } catch (error) {
+    return (error as AxiosError).response?.data
+  }
 }
-export async function onDeleteRedirectAction(connectionId: string, domain:string) {
-  const url = apiUrl("v1/connections/" + connectionId + "/redirects")
-  const res = await fetch(url, {
-    method: "DELETE", headers: { "Content-Type": "application/json" }, body: JSON.stringify({
-      Domain:domain
-    })
-  })
+
+export async function onDeleteRedirectAction(connectionId: string, domain: string[]) {
+  const url = apiUrl(`v1/connections/${connectionId}/redirects`)
+  let responses
+  for (let dom of domain) {
+    try {
+      const res = await apiAxios.delete(url, {
+        headers: { "Content-Type": "application/json" },
+        data: { Domain: dom },
+      })
+      responses = res.data
+    } catch (error) {
+      return (error as AxiosError).response?.data
+    }
+  }
+  return responses
 }
 
 export async function onGetConfigHash(connectionId: string) {
-  const url = apiUrl("v1/connections/" + connectionId + "/confighash")
-  const res = await fetch(url, {
-    method: "GET"
-  })
-  return await res.json() as {Data:{Hash:string}}
+  const url = apiUrl(`v1/connections/${connectionId}/confighash`)
+  try {
+    const res = await apiAxios.get(url)
+    return await res.data
+  } catch (error) {
+    return (error as AxiosError).response?.data
+  }
 }
 
-
 export async function onReloadActions(connectionId: string) {
-  const res = await fetch(apiUrl(`v1/connections/${connectionId}/reload`), { method: "POST" })
+  const url = apiUrl(`v1/connections/${connectionId}/reload`)
+  try {
+    const res = await apiAxios.post(url)
+    return await res.data
+  } catch (error) {
+    return (error as AxiosError).response?.data
+  }
 }
