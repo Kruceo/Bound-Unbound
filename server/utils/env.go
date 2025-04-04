7e8223eb5816321159c7926a3bc738f10519ce37
kruceo <kruceo.com@gmail.com>
2025-04-04 16:34:57 -0300

    wip: fix env utils

--- server/utils/env.go
+++ server/utils/env.go
@@ -1,6 +1,9 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 func GetEnvOrDefault(key, defaultValue string) string {
 	value, exists := os.LookupEnv(key)
@@ -8,4 +11,16 @@ func GetEnvOrDefault(key, defaultValue string) string {
 		return defaultValue
 	}
 	return value
+}
+
+func GetEnvOrDefaultNumber(key string, defaultValue int64) int64 {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	n, err := strconv.ParseInt(value, 10, 32)
+	if err != nil {
+		return defaultValue
+	}
+	return n
 }
