e780d704db348262d1f64a45d8e3412a53d43a38
kruceo <kruceo.com@gmail.com>
2025-04-15 13:17:58 -0300

    wip: include endpoints in auth router

--- server/application/presentation/routers/v1authRouter.go
+++ server/application/presentation/routers/v1authRouter.go
@@ -7,16 +7,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func SetupAuthRouter(r *mux.Router, userRepo infrastructure.UserRepository, sessionSecret string) *mux.Router {
+func SetupAuthRouter(r *mux.Router, userRepo infrastructure.UserRepository, roleRepo infrastructure.RoleRepository, sessionSecret string) *mux.Router {
 
-	authController := v1.NewV1AuthHandlers(userRepo, sessionSecret)
+	authController := v1.NewV1AuthHandlers(userRepo, roleRepo, sessionSecret)
 
 	router := r.PathPrefix("/auth").Subrouter()
 	router.HandleFunc("/login", authController.AuthLoginHandler).Methods("POST")
 	router.HandleFunc("/token", authController.AuthClientToken).Methods("GET")
 	router.HandleFunc("/register", authController.AuthRegisterHandler).Methods("POST")
 	router.HandleFunc("/register/request", authController.AuthCreateRegisterRequest).Methods("POST")
-	// router.HandleFunc("/status", authController.AuthHasUserHandler).Methods("GET")
+
+	router.HandleFunc("/roles", authController.AuthGetRoles).Methods("GET")
+	router.HandleFunc("/roles", authController.AuthPostRole).Methods("POST")
+	router.HandleFunc("/roles", authController.AuthDeleteRole).Methods("DELETE")
+
 	router.HandleFunc("/reset", authController.AuthResetAccountHandler).Methods("POST")
 	router.HandleFunc("/reset/pwd", authController.AuthResetAccountPasswordHandler).Methods("POST")
 
