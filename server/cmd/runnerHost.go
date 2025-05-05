a72093e1b7e1bb2853c5b06c32b7f83d45337e28
kruceo <kruceo.com@gmail.com>
2025-05-05 16:35:41 -0300

    wip: include on runner

--- server/cmd/runnerHost.go
+++ server/cmd/runnerHost.go
@@ -23,23 +23,37 @@ func Run() {
 	responseRepo := adapters.NewInMemoryResponseRepository()
 	userRepo := adapters.NewFileSystemUserRepo("users.temp.json")
 	roleRepo := adapters.NewFileRoleRepository("roles.temp.json")
+	nodeRoleBindRepo, _ := adapters.NewFileNodeRoleBindRepository("binds.temp.json")
 
 	jwtUseCase := usecases.NewJWTUseCase(enviroment.SESSION_SECRET)
 
 	authMiddleware := middlewares.NewJWTMiddleware(enviroment.SESSION_SECRET).AuthMiddleware
 	corsMiddleware := middlewares.NewCorsMiddleware(enviroment.CORS_ORIGIN, "Authorization", "Content-Type", "Cookie").CorsMiddleware
 	bforceMiddleware := middlewares.NewBruteForceMiddleware().BruteForceMiddleware
-	roleMiddleware := middlewares.NewRoleMiddleware([]string{"admin"}, jwtUseCase, userRepo, roleRepo).Middleware
+	adminPermMiddleware := middlewares.NewRoleMiddleware([]string{"admin"}, jwtUseCase, userRepo, roleRepo).Middleware
+	anyPermMiddleware := middlewares.NewRoleMiddleware([]string{"*"}, jwtUseCase, userRepo, roleRepo).Middleware
 
 	r := mux.NewRouter()
-	apiRouter := routers.SetupNodesRouter(r, &nodeRepo, &responseRepo)
+	apiRouter := routers.SetupNodesRouter(r, &nodeRepo, &responseRepo, nodeRoleBindRepo, roleRepo)
 	loginRouter := routers.SetupAuthRouter(r, userRepo, roleRepo, jwtUseCase)
-	adminRouter := routers.SetupAdminRouter(r, userRepo, roleRepo, jwtUseCase)
+	adminRouter := routers.SetupAdminRouter(r, userRepo, roleRepo, &nodeRepo, nodeRoleBindRepo, jwtUseCase)
 	routers.SetupWebsocketRouter(r, &nodeRepo, &responseRepo, priv, pub)
 
-	apiRouter.Use(corsMiddleware, authMiddleware)
-	loginRouter.Use(corsMiddleware, bforceMiddleware)
-	adminRouter.Use(corsMiddleware, authMiddleware, roleMiddleware)
+	apiRouter.Use(
+		corsMiddleware,
+		authMiddleware,
+		anyPermMiddleware,
+	)
+	loginRouter.Use(
+		corsMiddleware,
+		// anyPermMiddleware,
+		bforceMiddleware,
+	)
+	adminRouter.Use(
+		corsMiddleware,
+		authMiddleware,
+		adminPermMiddleware,
+	)
 	port := utils.GetEnvOrDefaultNumber("port", 8080)
 	fmt.Println("listening", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
